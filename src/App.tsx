481ff5976bf9b3e46ac2dbcc31a49ecb2286f06c
supermassive-jh <supermassive_jh@naver.com>
2026-07-28 15:51:18 +0900

    fix: scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import RootLayout from "./layout/RootLayout"
 import BoardPage from "./pages/BoardPage"
 import PostPage from "./pages/PostPage"
+import { ScrollToTop } from "./components/scroll-to-top"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<RootLayout />}>
           <Route index element={<BoardPage />} />
